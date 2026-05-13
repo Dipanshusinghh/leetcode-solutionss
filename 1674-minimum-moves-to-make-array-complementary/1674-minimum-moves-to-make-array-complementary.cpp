45b40e58caf9a9ea8e864edb4e5c683640ba83af
Dipanshu singh <161134993+Dipanshusinghh@users.noreply.github.com>
2026-05-13 15:48:22 +0530

    Time: 8 ms (66.37%), Space: 93.5 MB (43.36%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -3,15 +3,18 @@ public:
     int minMoves(vector<int>& nums, int limit) {
         int n = nums.size();
         vector<int> diff(2 * limit + 2, 0);
+
         for (int i = 0; i < n / 2; ++i) {
             int a = min(nums[i], nums[n - 1 - i]);
             int b = max(nums[i], nums[n - 1 - i]);
+
             diff[2] += 2;
             diff[a + 1] -= 1;
             diff[a + b] -= 1;
             diff[a + b + 1] += 1;
             diff[b + limit + 1] += 1;
         }
+
         int min_ops = n;
         int current_ops = 0;
 
@@ -19,6 +22,7 @@ public:
             current_ops += diff[c];
             min_ops = min(min_ops, current_ops);
         }
+
         return min_ops;
     }
 };
